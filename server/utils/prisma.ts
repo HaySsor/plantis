7010a9b6a004e17860704f769965996aa23fcc38
przemekhussar <Hayss007@gmail.com>
2026-03-29 19:17:05 +0200

    Limit connection pool to 1 for serverless Vercel deployment

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -9,6 +9,6 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is missing");
 }
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString, max: 1 });
 
 export const prisma = new PrismaClient({ adapter });
